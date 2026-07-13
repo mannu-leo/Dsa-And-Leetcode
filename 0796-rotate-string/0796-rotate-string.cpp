db6412c0f5ebd6bce69c40685effb4812193d192
Manas Dobal <manasdobal82@gmail.com>
2026-07-13 23:44:09 +0530

    Time: 0 ms (100%), Space: 8.1 MB (75.84%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -8,20 +8,22 @@ public:
         if(s==goal){
             return true;
         }
-        int j=0;
-        char temp;
-        while(j<n){
-            temp=s[0];
-            for(int i=1;i<n;i++){
-                s[i-1]=s[i];
-            }
-            j++;
-            s[n-1]=temp;
-            if(s==goal){
-                return true;
-            }
-        }
-        return false;
+        // int j=0;
+        // char temp;
+        // while(j<n){
+        //     temp=s[0];
+        //     for(int i=1;i<n;i++){
+        //         s[i-1]=s[i];
+        //     }
+        //     j++;
+        //     s[n-1]=temp;
+        //     if(s==goal){
+        //         return true;
+        //     }
+        // }
+        // return false;o(nxn) approach
+        string temp=s+s;//we concadenate the string so that all the combination will be made up
+        return temp.find(goal)!=string::npos;//check whether the goal is in temp or not
         
     }
 };
