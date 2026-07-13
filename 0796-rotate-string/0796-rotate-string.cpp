5737f225a91e2f0bd4a1576ed46f82f2ecead5b5
Manas Dobal <manasdobal82@gmail.com>
2026-07-13 23:14:29 +0530

    Time: 0 ms (100%), Space: 8.1 MB (49.61%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=s.size();
+        if(s.size()!=goal.size()){
+            return false;
+        }
+        if(s==goal){
+            return true;
+        }
+        int j=0;
+        char temp;
+        while(j<n){
+            temp=s[0];
+            for(int i=1;i<n;i++){
+                s[i-1]=s[i];
+            }
+            j++;
+            s[n-1]=temp;
+            if(s==goal){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
