c9f921d4e5d9113e0920a8d0b20c2ca3fd10d153
Manas Dobal <manasdobal82@gmail.com>
2026-07-16 23:27:41 +0530

    Time: 0 ms (100%), Space: 7.8 MB (21.81%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
-
     bool judgeSquareSum(int c) {
-        for(int i=0;i<=sqrt(c);i++){
-            long long a=1LL*i*i;
-            int low=i;
-            int high=c;
-            while(low<=high){
-                long long mid =low+(high-low)/2;
-                if(mid*mid==c-a){
-                    return true;
-                }
-                else if(mid*mid>c-a){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
+        // for(int i=0;i<=sqrt(c);i++){
+        //     long long a=i*i;
+        //     int low=i;
+        //     int high=c;
+        //     while(low<=high){
+        //         long long mid =low+(high-low)/2;
+        //         if(mid*mid==c-a){
+        //             return true;
+        //         }
+        //         else if(mid*mid>c-a){
+        //             high=mid-1;
+        //         }
+        //         else{
+        //             low=mid+1;
                     
-                }
-            }
+        //         }
+        //     }
             
-        }        
+        // }        
+        // return false;O(nlogn)
+        long long a=0;
+        long long b=sqrt(c);
+        long long sum;
+        while(a<=b){
+            sum=a*a+b*b;
+            if(sum==c){
+                return true;
+            }
+            else if(sum>c){
+                b--;
+            }
+            else{
+                a++;
+            }
+
+        }
         return false;
-        
     }
 };
