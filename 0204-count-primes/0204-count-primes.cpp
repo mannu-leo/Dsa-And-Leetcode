d71108b30f782f5fc348623a1531191e46714a39
Manas Dobal <manasdobal82@gmail.com>
2026-07-17 18:04:24 +0530

    Time: 175 ms (54.29%), Space: 132.7 MB (8.97%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 0;
+        }
+        int count=0;
+        vector<int>check(n,0);
+        for(int i=2;i<n;i++){
+            if(check[i]!=0){
+                continue;
+            }
+            else{
+                count++;
+            }
+            long long x=0;
+            int j=2;
+            while(x<n){
+                x=1LL*j*i;
+                if(x>=n){
+                    break;
+                }
+                check[x]=1;
+                j++;
+            }
+        }
+        return count;
+        
+    }
+};
