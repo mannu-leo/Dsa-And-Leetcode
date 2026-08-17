33158556f16adf62d2ff74d17f50769b60150372
Manas Dobal <manasdobal82@gmail.com>
2026-08-17 16:25:03 +0530

    Time: 86 ms (30.88%), Space: 174.2 MB (10.74%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    string pallindrome(string s,int left,int right){
-        int maxlen=0;
+    string palindrome(string s,int left,int right){
         int start=0;
+        int maxlen=0;
         while(left>=0&&right<s.size()&&s[left]==s[right]){
             if(right-left+1>maxlen){
-                start=left;
                 maxlen=right-left+1;
+                start=left;
             }
             left--;
             right++;
@@ -15,25 +15,18 @@ public:
     }
     string longestPalindrome(string s) {
         int n=s.size();
-        int maxlen=0;
-        int start=0;
         string ans;
         for(int i=0;i<n;i++){
-            int left=i;
-            int right=i+1;
-            string s1=pallindrome(s,i,i);
-            string s2=pallindrome(s,i,i+1);
-            if(s1.size()>maxlen){
+            string s1=palindrome(s,i,i);
+            string s2=palindrome(s,i,i+1);
+            if(s1.size()>ans.size()){
                 ans=s1;
-                maxlen=s1.size();
             }
-            if(s2.size()>maxlen){
+            if(s2.size()>ans.size()){
                 ans=s2;
-                maxlen=s2.size();
-            }     
-            
+            }
+
         }
-        return ans;
-        
+        return ans;       
     }
 };
